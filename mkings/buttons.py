1a1b23347a70af4876803e94a26508a4a5243a90
Gustavo Tontini <gustavotontini@gmail.com>
2020-11-15 03:29:19 +0000

    Different Button types as methods of the same obj.

--- mkings/buttons.py
+++ mkings/buttons.py
@@ -4,9 +4,11 @@ import pygame
 class Button:
     '''
     A Button is used to call a function/method when clicked.
-    - button(surface) method prints the Button as normal.
-    - lock_button(surface, condition) method prints the Button
+    - button(surface) method prints a normal Button.
+    - switch(surface, condition) method prints a Switch
         that stays pressed while a "condition" is True.
+    - toggle(surface, condition) method prints a Toggle
+        that flips left and right depending of a "condition".
     '''
     def __init__(
         self,
@@ -67,10 +69,21 @@ class Button:
             surface.blit(*rest)
             self._call_function()
 
-    def lock_button(self, surface, condition):
+    def switch(self, surface, condition):
         """
         Prints on the Surface a button that stays pressed 
         if a condition is True.
+
+        # EXAMPLE #
+        1) Create the Switch:
+        button = Button(50,50,20,20,"rest.png","pressed.png",choose_attribute,value))
+
+        2) Define the Function:
+        def choose_attribute(self, value):
+            attribute = value
+
+        3) Place the Switch:
+        tog.switch(SURFACE, (attribute == "value"))
         """
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
@@ -89,90 +102,36 @@ class Button:
             surface.blit(*rest)
             self._call_function()
 
+    def toggle(self, surface, condition):
+        '''
+        A Toggle is used to choose between two options.
+        It should be linked to a function or method that changes 
+        a given attribute (or global variable) value.
 
-class Toggle:
-    '''
-    A Toggle is used to choose between two options.
-    It should be linked to a function or method that changes 
-    a given attribute (or global variable) value.
+        # EXAMPLE #
+        1) Create the Toggle:
+        button = Button(50,50,20,20,"toggle_left.png","toggle_right.png",choose_attribute))
 
-    # EXAMPLE #
-    1) Create the Toggle:
-    tog = Toggle(50,50,20,20,choose_attribute))
+        2) Define the Function:
+        def choose_attribute(self):
+            if attribute == option_1:
+                attribute = option_2
+            else:
+                attribute = option_1
 
-    2) Define the Function:
-    def choose_attribute(self):
-        if attribute == option_1:
-            attribute = option_2
-        else:
-            attribute = option_1
-
-    3) Place the Button:
-    tog.switch(SURFACE)
-    '''
-    def __init__(
-        self,
-        center_x,
-        center_y,
-        width,
-        height,
-        action_func=None,
-        togleft_image="toggle_left.png",
-        togright_image="toggle_right.png",
-        state="left",
-        path = "images/",
-    ):
-
-        self.center_x = int(center_x)
-        self.center_y = int(center_y)
-        self.width = int(width)
-        self.height = int(height)
-        self.action_func = action_func
-        self.togleft_image = togleft_image
-        self.togright_image = togright_image
-        self.state = state
-        self.path = path
-
-    def _get_image(self):
-        """
-        Returns a tuple with the image and image_rect
-        of the current Toggle state.
-        """
-        if self.state == "left":
-            image_path = self.path + self.togleft_image
-        elif self.state == "right":
-            image_path = self.path + self.togright_image
-        image = pygame.image.load(image_path).convert_alpha()
-        scaled_image = pygame.transform.scale(image, (self.width, self.height))
-        image_rect = scaled_image.get_rect()
-        image_rect.center = (self.center_x, self.center_y)
-        return scaled_image, image_rect
-
-    def _change_state(self):
-        """Changes the current state."""
-        if self.state == "right":
-            self.state = "left"
-        else:
-            self.state = "right"
-
-    def _call_function(self):
-        """Calls the function that switches the option."""
-        if self.action_func is not None:
-            self.action_func()
-      
-    def switch(self, surface):
-        """
-        Prints the Toggle Switch with its functionality
-        on the Surface.
-        """
+        3) Place the Toggle:
+        tog.toggle(SURFACE, (attribute == "option 2"))
+        '''
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
 
-        image = self._get_image() # Tuple (image, image_rect)
-        surface.blit(*image)
+        left = self._get_image()
+        right = self._get_image("hover")
 
-        if image[1].collidepoint(mouse) and click[0] == 1:
-            self._change_state()
+        if condition is True:
+            surface.blit(*right)
+        else:
+            surface.blit(*left)
+
+        if (left[1].collidepoint(mouse) and click[0] == 1):
             self._call_function()
-            image = self._get_image()
-            surface.blit(*image)
--- mkings/screens.py
+++ mkings/screens.py
@@ -13,7 +13,7 @@ def _text(font, text_input, x_center, y_center, color=WHITE):
 
 
 def start_menu(game, display):
-    HINT = display.HINT*1.3
+    HINT = display.HINT * 1.3
     DISP_W = display.DISP_W
     DISP_H = display.DISP_H
 
@@ -64,18 +64,22 @@ def start_menu(game, display):
         game.choose_players,
         4,
     )
-    grid = Toggle(
+    grid = Button(
         DISP_W * 0.5,
         DISP_H * 0.5 - HINT * 0.5,
         HINT * 0.6,
         HINT * 0.2,
+        "toggle_left.png",
+        "toggle_right.png",
         game.choose_grid,
     )
-    setup = Toggle(
+    setup = Button(
         DISP_W * 0.5,
         DISP_H * 0.5 + HINT * 0.2,
         HINT * 0.6,
         HINT * 0.2,
+        "toggle_left.png",
+        "toggle_right.png",
         game.choose_setup,
     )
     logo = Button(
@@ -89,32 +93,21 @@ def start_menu(game, display):
     )
 
     # TEXTS
-    t1 = _text(
-        DIMBO_L, "Number of Players", DISP_W * 0.5, DISP_H * 0.5 - HINT * 1.7
-        )
-    t2 = _text(
-        DIMBO_L, "Grid Size", DISP_W * 0.5, DISP_H * 0.5 - HINT * 0.8
-        )
-    t21 = _text(
-        DIMBO_R, "5x5", DISP_W * 0.5 - HINT * 0.5, DISP_H * 0.5 - HINT * 0.5
-        )
-    t22 = _text(
-        DIMBO_R, "6x6", DISP_W * 0.5 + HINT * 0.5, DISP_H * 0.5 - HINT * 0.5
-        )
-    t3 = _text(
-        DIMBO_L, "Setup Variant", DISP_W * 0.5, DISP_H * 0.5 - HINT * 0.1
-        )
+    t1 = _text(DIMBO_L, "Number of Players", DISP_W * 0.5, DISP_H * 0.5 - HINT * 1.7)
+    t2 = _text(DIMBO_L, "Grid Size", DISP_W * 0.5, DISP_H * 0.5 - HINT * 0.8)
+    t21 = _text(DIMBO_R, "5x5", DISP_W * 0.5 - HINT * 0.5, DISP_H * 0.5 - HINT * 0.5)
+    t22 = _text(DIMBO_R, "6x6", DISP_W * 0.5 + HINT * 0.5, DISP_H * 0.5 - HINT * 0.5)
+    t3 = _text(DIMBO_L, "Setup Variant", DISP_W * 0.5, DISP_H * 0.5 - HINT * 0.1)
     t31 = _text(
         DIMBO_R, "Standard", DISP_W * 0.5 - HINT * 0.7, DISP_H * 0.5 + HINT * 0.2
-        )
+    )
     t32 = _text(
         DIMBO_R, "Alternate", DISP_W * 0.5 + HINT * 0.7, DISP_H * 0.5 + HINT * 0.2
-        )
-    t4 = _text(
-        UBUNTU_R, "v0.6 made by G. Scary T.", DISP_W * 0.86, DISP_H * 0.97
-        )
+    )
+    t4 = _text(UBUNTU_R, "v0.6 made by G. Scary T.", DISP_W * 0.86, DISP_H * 0.97)
     start_menu_text = t1, t2, t21, t22, t3, t31, t32, t4
 
+    # SCREEN LOOP
     while game._creating:
         clock.tick(FPS)
 
@@ -124,12 +117,12 @@ def start_menu(game, display):
             for text in start_menu_text:
                 display.WINDOW.blit(*text)
 
-            solo.lock_button(display.WINDOW, (game._num_of_players == 2))
-            two.lock_button(display.WINDOW, (game._num_of_players == 3))
-            three.lock_button(display.WINDOW, (game._num_of_players == 4))
-            four.lock_button(display.WINDOW, (game._num_of_players == 5))
-            grid.switch(display.WINDOW)
-            setup.switch(display.WINDOW)
+            solo.switch(display.WINDOW, (game._num_of_players == 2))
+            two.switch(display.WINDOW, (game._num_of_players == 3))
+            three.switch(display.WINDOW, (game._num_of_players == 4))
+            four.switch(display.WINDOW, (game._num_of_players == 5))
+            grid.toggle(display.WINDOW, (game._grid_size == (6,6)))
+            setup.toggle(display.WINDOW, (game._setup_variant == "alternate"))
             logo.button(display.WINDOW)
 
             if event.type == pygame.QUIT:
@@ -143,59 +136,66 @@ def start_menu(game, display):
             pygame.display.update()
 
 
-def end_screen(display, game):
+def end_screen(game, display):
     _end_screen = True
-    HINT = display.HINT*1.3
-    DISP_W = display.DISP_W
-    DISP_H = display.DISP_H
-
-    pygame.font.init()
-    DIMBO_L = pygame.font.Font(FONTS_PATH + "dimbo_regular.ttf", int(HINT*0.5))
-    DIMBO_R = pygame.font.Font(FONTS_PATH + "dimbo_regular.ttf", int(HINT*0.2))
-
-    if game._winner is not None:
-        if Player.total == 2:
-            if game._winner == game.counter:
-                t1 = _text(
-                    DIMBO_L, "You Lost!", DISP_W*0.5, DISP_H*0.5 - HINT*0.3
-                )
-                t2 = _text(
-                    DIMBO_R,
-                    f"The Counter King recruited {game._winner.score} {'Pairs' if game._winner.score != 1 else 'Pair'}",
-                    DISP_W*0.5,
-                    DISP_H*0.5 + HINT*0.3,
-                )
-                t3 = _text(
-                    DIMBO_R,
-                    f"and reached the Card on {game._winner.pawn[0].col+1}, {game._winner.pawn[0].row+1}.",
-                    DISP_W*0.5,
-                    DISP_H*0.5 + HINT*0.55,
-                )
-            else:
-                t1 = _text(
-                    DIMBO_L, "You Won!", DISP_W*0.5, DISP_H*0.5 - HINT*0.3
-                )
-                t2 = _text(
-                    DIMBO_R,
-                    f"You recruited {game._winner.score} {'Pairs' if game._winner.score != 1 else 'Pair'}.",
-                    DISP_W*0.5,
-                    DISP_H*0.5 + HINT*0.3,
-                )
-        else:
-            t1 = _text(
-                DIMBO_L,
-                f"Player {game._winner.color} Won!",
-                DISP_W*0.5,
-                DISP_H*0.5 - HINT*0.3,
-            )
-            t2 = _text(
-                DIMBO_R,
-                f"{game._winner.score} {'Pairs' if game._winner.score != 1 else 'Pair'} Recruited.",
-                DISP_W*0.5,
-                DISP_H*0.5 + HINT*0.3,
-            )
 
     while _end_screen:
+
+        # ABBREVIATIONS
+        HINT = display.HINT * 1.3 # Magic number adjusts sizes without messing positions.
+        DISP_W = display.DISP_W
+        DISP_H = display.DISP_H
+
+        pygame.font.init()
+        DIMBO_L = pygame.font.Font(FONTS_PATH + "dimbo_regular.ttf", int(HINT * 0.5))
+        DIMBO_R = pygame.font.Font(FONTS_PATH + "dimbo_regular.ttf", int(HINT * 0.2))
+        
+        # TEXTS
+        if game._winner is not None:
+            if Player.total == 2:
+                if game._winner == game.counter:
+                    t1 = _text(
+                        DIMBO_L, "You Lost!", DISP_W * 0.5, DISP_H * 0.5 - HINT * 0.4
+                    )
+                    t2 = _text(
+                        DIMBO_R,
+                        f"The Counter King recruited {game._winner.score}"
+                        f" {'Pairs' if game._winner.score != 1 else 'Pair'}",
+                        DISP_W * 0.5,
+                        DISP_H * 0.5 + HINT * 0.3,
+                    )
+                    t3 = _text(
+                        DIMBO_R,
+                        f"and reached the Card on {game._winner.pawn[0].col+1},"
+                        f" {game._winner.pawn[0].row+1}.",
+                        DISP_W * 0.5,
+                        DISP_H * 0.5 + HINT * 0.55,
+                    )
+                else:
+                    t1 = _text(DIMBO_L, "You Won!", DISP_W * 0.5, DISP_H * 0.5 - HINT * 0.3)
+                    t2 = _text(
+                        DIMBO_R,
+                        f"You recruited {game._winner.score}"
+                        f" {'Pairs' if game._winner.score != 1 else 'Pair'}.",
+                        DISP_W * 0.5,
+                        DISP_H * 0.5 + HINT * 0.3,
+                    )
+            else:
+                t1 = _text(
+                    DIMBO_L,
+                    f"Player {game._winner.color} Won!",
+                    DISP_W * 0.5,
+                    DISP_H * 0.5 - HINT * 0.3,
+                )
+                t2 = _text(
+                    DIMBO_R,
+                    f"{game._winner.score}"
+                    f" {'Pairs' if game._winner.score != 1 else 'Pair'} Recruited.",
+                    DISP_W * 0.5,
+                    DISP_H * 0.5 + HINT * 0.3,
+                )
+
+        # ACTUAL SCREEN
         if game._winner is not None:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -204,7 +204,7 @@ def end_screen(display, game):
                 if event.type == pygame.VIDEORESIZE:
                     size = pygame.display.get_window_size()
                     display._resize(game.board, size)
-                    
+
                 display.WINDOW.fill((BACKGROUND))
                 display.WINDOW.blit(*t1)
                 display.WINDOW.blit(*t2)
@@ -212,7 +212,6 @@ def end_screen(display, game):
                     display.WINDOW.blit(*t3)
                 except UnboundLocalError:
                     pass
-
             pygame.display.update()
 
         else:
